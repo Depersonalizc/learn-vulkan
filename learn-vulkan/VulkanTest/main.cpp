6a50c38fb3cf93adcea2525304a294cdf351f583
Depersonalizc <1046770165@qq.com>
2023-06-11 01:49:30 -0700

    fixed functions

--- learn-vulkan/VulkanTest/main.cpp
+++ learn-vulkan/VulkanTest/main.cpp
@@ -83,6 +83,9 @@ private:
     VkFormat swapChainImageFormat;          // format of the surface associated with the swap chain
     VkExtent2D swapChainExtent;
 
+    /* Pipeline */
+    VkPipelineLayout pipelineLayout;
+
 private:
     /* Main Routines */
     void initWindow() {
@@ -117,6 +120,8 @@ private:
 
     void cleanup() {
         std::cout << "Performing cleanup...\n";
+
+        vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         
         for (const auto& imageView : swapChainImageViews)
             vkDestroyImageView(device, imageView, nullptr);
@@ -532,27 +537,126 @@ private:
         auto vertShaderSpv = readFile("VulkanTest/shaders/spv/triangle_vert.spv");
         auto fragShaderSpv = readFile("VulkanTest/shaders/spv/triangle_frag.spv");
 
+        // Shader modules: Thin wrapper around the spir-v byte code
         VkShaderModule vertShaderModule = createShaderModule(vertShaderSpv);
         VkShaderModule fragShaderModule = createShaderModule(fragShaderSpv);
 
-        VkPipelineShaderStageCreateInfo shaderStageCreateInfos[] = {
-            // Vertex shader stage
-            {
-                .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-                .stage = VK_SHADER_STAGE_VERTEX_BIT,
-                .module = vertShaderModule,
-                .pName = "main",
-            },
-            // Fragment shader stage
-            {
-                .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-                .stage = VK_SHADER_STAGE_FRAGMENT_BIT,
-                .module = fragShaderModule,
-                .pName = "main",
-            },
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+            .stage = VK_SHADER_STAGE_VERTEX_BIT,
+            .module = vertShaderModule,
+            .pName = "main",
+        };
+        VkPipelineShaderStageCreateInfo fragShaderStageInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+            .stage = VK_SHADER_STAGE_FRAGMENT_BIT,
+            .module = fragShaderModule,
+            .pName = "main",
+        };
+        VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+        /* Fix-function modules */
+
+        // Vertex Input: Specify format of vertex data, including
+        //  - Binding: spacing, whether data is per-vertex or per-instance
+        //  - Attributes: Types of attributes, which binding to load them and at which offset
+        VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+        vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertexInputInfo.vertexBindingDescriptionCount = 0;
+        vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optional
+        vertexInputInfo.vertexAttributeDescriptionCount = 0;
+        vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optional
+
+        // Input Assembly: 
+        // - What kind of geometry will be drawn from the vertices (topology)
+        // - Primitive restart?
+        VkPipelineInputAssemblyStateCreateInfo inputAssembly{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+            .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+            .primitiveRestartEnable = VK_FALSE,
         };
 
-        // TODO: fixed-function stages
+        // Viewport and Scissors: Make them dynamic state
+        std::vector<VkDynamicState> dynamicStates{
+            VK_DYNAMIC_STATE_VIEWPORT,
+            VK_DYNAMIC_STATE_SCISSOR,
+        };
+        // Since these are dynamic state, we only need to specify count at creation time 
+        // w/o passing in the createInfo. Will be actually set up at drawing time
+        VkPipelineDynamicStateCreateInfo dynamicStateInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
+            .dynamicStateCount = static_cast<uint32_t>(dynamicStates.size()),
+            .pDynamicStates = dynamicStates.data(),
+        };
+        VkPipelineViewportStateCreateInfo viewportStateInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+            .viewportCount = 1,
+            .scissorCount = 1,
+        };
+
+        // Rasterizer
+        VkPipelineRasterizationStateCreateInfo rasterizerInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+            .depthClampEnable = VK_FALSE,          // Do fragments beyond viewport min-max depth get clamped, or discarded?
+            .rasterizerDiscardEnable = VK_FALSE,   // Disable output to the framebuffer?
+            .polygonMode = VK_POLYGON_MODE_FILL,
+            .cullMode = VK_CULL_MODE_BACK_BIT,
+            .frontFace = VK_FRONT_FACE_CLOCKWISE,
+            .depthBiasEnable = VK_FALSE,           // depth bias (disbale for now)
+            //.depthBiasConstantFactor = 0.0f,       // Optional
+            //.depthBiasClamp = 0.0f,                // Optional
+            //.depthBiasSlopeFactor = 0.0f,          // Optional
+            .lineWidth = 1.0f,
+        };
+
+        // Multisampling (disable for now)
+        VkPipelineMultisampleStateCreateInfo multisampling{};
+        multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisampling.sampleShadingEnable = VK_FALSE;
+        multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+        multisampling.minSampleShading = 1.0f; // Optional
+        multisampling.pSampleMask = nullptr; // Optional
+        multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
+        multisampling.alphaToOneEnable = VK_FALSE; // Optional
+
+        // Color blending (disable for now)
+        // Config per attached framebuffer
+        VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+        colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+        colorBlendAttachment.blendEnable = VK_FALSE;
+        colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+        colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+        colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD; // Optional
+        colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+        colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+        colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD; // Optional
+        // Global config for all framebuffers. Will disable the blending specified per attached framebuffer
+        VkPipelineColorBlendStateCreateInfo colorBlending{};
+        colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        colorBlending.logicOpEnable = VK_FALSE;
+        colorBlending.logicOp = VK_LOGIC_OP_COPY; // Optional
+        colorBlending.attachmentCount = 1;
+        colorBlending.pAttachments = &colorBlendAttachment;
+        colorBlending.blendConstants[0] = 0.0f; // Optional
+        colorBlending.blendConstants[1] = 0.0f; // Optional
+        colorBlending.blendConstants[2] = 0.0f; // Optional
+        colorBlending.blendConstants[3] = 0.0f; // Optional
+
+        // Pipeline layout: Specify uniforms
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+            .setLayoutCount = 0,                // Optional
+            .pSetLayouts = nullptr,             // Optional
+            .pushConstantRangeCount = 0,        // Optional
+            .pPushConstantRanges = nullptr,     // Optional
+        };
+
+        VkResult res = vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout);
+        if (res != VK_SUCCESS) {
+            throw std::runtime_error(
+                std::string("failed to create pipeline layout, error ") + string_VkResult(res)
+            );
+        }
 
         vkDestroyShaderModule(device, vertShaderModule, nullptr);
         vkDestroyShaderModule(device, fragShaderModule, nullptr);
